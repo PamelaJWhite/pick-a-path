28eb7bcb556f124aa6a1813603d87ce930eea9d6
pjwhite08 <pamelajeanwhite@gmail.com>
2022-01-23 11:33:24 -0600

    styling started; scroll working

--- src/components/UserStories.js
+++ src/components/UserStories.js
@@ -7,43 +7,50 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Container from '@mui/material/Container';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Zoom from '@mui/material/Zoom';
+import IconButton from '@mui/material/IconButton';
+import HomeIcon from '@mui/icons-material/Home';
+import {
+    DataGridPro,
+    useGridApiRef,
+    gridVisibleRowCountSelector,
+    visibleGridColumnsLengthSelector,
+    visibleGridColumnsSelector,
+    gridVisibleSortedRowIdsSelector,
+} from '@mui/x-data-grid-pro';
+import { useDemoData } from '@mui/x-data-grid-generator';
+import Slide from '@mui/material/Slide';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 //import context
 import { Context } from "../context/Context";
 import { getDividerUtilityClass } from "@mui/material";
 
-//create styles for this component
-const useStyles = makeStyles({
-    formContainer: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
+window.history.pushState(null, null, window.location.href);
+window.onpopstate = function () {
+window.history.go(1);
+};
+
+const theme = createTheme({
+    typography: {
+        h1: {
+            fontSize: 20,
         },
-    foaasResponse: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "flex-end",
-    },
-    textField: {
-        width: "100%",
-    },
-    spacer: {
-        height: 15,
     },
 });
 
-    window.history.pushState(null, null, window.location.href);
-    window.onpopstate = function () {
-    window.history.go(1);
-    };
-const UserStories = () => {
-    
-    //create variable name to access styles
-    // const classes = useStyles();
-
-    //destructure context for only the states and functions needed
+export default function UserStories (props) {
+        //destructure context for only the states and functions needed
     const {
         getAllStoryTitles,
         storyData,
@@ -59,130 +66,127 @@ const UserStories = () => {
         isSignedIn, 
         setIsSignedIn
     } = useContext(Context);
-
+    
     //!this can't stay in here
     //it's here bc I couldn't
     setIsSignedIn(true)
-
-    //when I had this getAllStoryTitles function here,
-    //outside the return
-    //I think it was calling hundreds of times
-    //as I worked
-    // getAllStoryTitles()
-
+    
     return (
-        <main className="{classes.formContainer}" style={{backgroundColor:"pink"}}>
-            <Box
-                sx={{
-                    display: 'flex',
-                    // flexWrap: 'wrap',
-                    '& > :not(style)': {
-                    m: 1,
-                    width: "49%",
-                    height: 400,
-                    },
-                }}
-    >
-        <Paper elevation={2}>
-            {storyData && (
-        <div>
-            <div variant="h3" component="div" sx={{ paddingBottom: 5 }}>
-                {storyData.map((element, idx)=>(
-                    <ul>
-                        <li 
-                            className="storyTitlesList"
-                            onClick={(e) => {
-                            let storyId = element.story_id
-                            console.log("wholeStory: ", wholeStory)
-                            postToMyStoryTitlesList(storyId) 
-                            }
-                        }>
-                            {element.title}
-                            </li>
-                    </ul>
-                ))}
+        <ThemeProvider theme={theme}>
+        <main style={{backgroundColor:"pink", display: "flex", flexDirection: "column", alignItems: "center"}} >
+            <div style={{ display:"flex", justifyContent: "center", border: "1px solid green"}}> 
+                <Box style={{ margin: "30px"}}>
+                    <Typography variant="h1">
+                        My Story Home
+                    </Typography>
+                </Box>
             </div>
-        </div>
-        )}
-        </Paper>
-        <Paper elevation={2}>
-            {myStoryTitles && (
-
-                <div>
-                    <div variant="h3" component="div" sx={{ paddingBottom: 5 }}>
-                        {myStoryTitles.map((element, idx)=>(
-                            <ul>
-                                <li 
-                                    className="storyTitlesList" 
-                        
-                                >
-                                    <Link 
-                                        onMouseDown={(e) =>{
-                                            setUserStoryId(element.user_story_id)
-                                        }}
-                                        onClick={(e) => {
-                                            //go to readCompleteStory first
-                                            //b/c we only want to read the first story section
-                                            //if there is no story already saved
-                                            readFirstStorySection(userStoryId)
-
-                                        }}
-                                        to={"/readingPage"}
-                                    >
-                                        {element.title}
-                                        
-                                    </Link>
-                                    <Grid container sx={{ color: 'text.primary' }}>
-                                        <Grid item xs={8}>
-                                            <DeleteForeverIcon 
-                                                className="deleteIcons"
+                <Box className = "titleBox">
+                    <div className = "paperBox">
+                        <Typography>
+                            All Stories
+                        </Typography>
+                        <Paper elevation={2} className="titlePaper" style={{marginRight: "20px" }}>
+                            <div className = "clickToAdd">
+                                <Typography>Click a Story to Add it to Your List</Typography>
+                            </div>
+                            {storyData && (
+                                <div variant="h3" component="div" sx={{ paddingBottom: 5 }}>
+                                    {storyData.map((element, idx)=>(
+                                        <ul>
+                                            <li 
+                                                className="storyTitlesList"
                                                 onClick={(e) => {
-                                                    let userStoryId = element.user_story_id
-                                                    // console.log("deleteTitle element.user_story_id and element.title: ", element.user_story_id, element.title)
-                                                    deleteTitle(userStoryId)
-                                                    }}
-                                            />
-                                        </Grid>
-                                    </Grid>
-                                    </li>
-                            </ul>
-                        ))}
+                                                let storyId = element.story_id
+                                                console.log("wholeStory: ", wholeStory)
+                                                postToMyStoryTitlesList(storyId) 
+                                                }}>
+                                                    {element.title}
+                                            </li>
+                                        </ul>
+                                    ))}
+                                </div>  
+                            )} 
+                        </Paper>
                     </div>
-                </div>
-            )}
-        </Paper>
-        </Box>
-            {/* this is set up as a button
-            because I think it was making hundreds of calls
-            when i had the get request functions 
-            run on loading */}
-            <Button
-                variant="contained"
-                style={{backgroundColor:"pink"}}
-                onClick={() => {
-                    // console.log("I'm clicked")
-                    getAllStoryTitles()
-                    getMyStoryTitles()
-                    }
-                }
-            >
-                click me
-            </Button> 
-            {/* <Button
-                variant="contained"
-                style={{backgroundColor:"pink"}}
-                onClick={() => {
-                    postToMyStoryTitlesList()
-                    }
-                }
-            >
-                post button
-            </Button> */}
-        </main>
-    );
-};
+                    <div className = "paperBox">
+                        <Typography>
+                            My Stories
+                        </Typography>
+                        <Paper elevation={2} className="titlePaper" style={{marginLeft: "20px"}}>
+                            <div className="clickToAdd">
+                                <Typography>Click to Read a Story</Typography>
+                            </div>
+                                {myStoryTitles && (
+                                    <div variant="h3" component="div" sx={{ paddingBottom: 5 }}>
+                                        {myStoryTitles.map((element, idx)=>(
+                                            <ul>
+                                                <li className="storyTitlesList">
+                                                    <Link 
+                                                        onMouseDown={(e) =>{
+                                                            setUserStoryId(element.user_story_id)
+                                                        }}
+                                                        onClick={(e) => {
+                                                            //go to readCompleteStory first
+                                                            //b/c we only want to read the first story section
+                                                            //if there is no story already saved
+                                                            readFirstStorySection(userStoryId)
 
-export default UserStories
+                                                        }}
+                                                        to={"/readingPage"}
+                                                    >
+                                                        {element.title}
+                                                        
+                                                    </Link>
+                                                    <Grid container sx={{ color: 'text.primary' }}>
+                                                        <Grid item xs={8}>
+                                                            <DeleteForeverIcon 
+                                                                className="deleteIcons"
+                                                                onClick={(e) => {
+                                                                    let userStoryId = element.user_story_id
+                                                                    // console.log("deleteTitle element.user_story_id and element.title: ", element.user_story_id, element.title)
+                                                                    deleteTitle(userStoryId)
+                                                                    }}
+                                                            />
+                                                        </Grid>
+                                                    </Grid>
+                                                </li>
+                                            </ul>
+                                        ))}
+                                    </div>
+                                )}
+                        </Paper>
+                    </div>
+                </Box>
+                {/* this is set up as a button
+                because I think it was making hundreds of calls
+                when i had the get request functions 
+                run on loading */}
+                <Button
+                    variant="contained"
+                    style={{backgroundColor:"pink"}}
+                    onClick={() => {
+                        // console.log("I'm clicked")
+                        getAllStoryTitles()
+                        getMyStoryTitles()
+                        }
+                    }
+                >
+                    click me
+                </Button> 
+        </main>
+        </ThemeProvider>
+    );
+}
+
+
+
+// export default UserStories
+
+
+
+
+//----------old stuff ---------
 
     // {/* Only displays text after successful api call
     //     {foaas && (
@@ -284,3 +288,147 @@ export default UserStories
     //     setThing("");
     //     setRandomNumber(Math.floor(Math.random() * 4) + 1);
     // };
+
+    //create styles for this component
+// const useStyles = makeStyles({
+//     formContainer: {
+//         display: "flex",
+//         flexDirection: "column",
+//         justifyContent: "center",
+//         },
+//     foaasResponse: {
+//         display: "flex",
+//         flexDirection: "column",
+//         justifyContent: "center",
+//         alignItems: "flex-end",
+//     },
+//     textField: {
+//         width: "100%",
+//     },
+//     spacer: {
+//         height: 15,
+//     },
+// });
+
+//create variable name to access styles
+    // const classes = useStyles();
+
+     //     //Why is this main acting like it's inside the first div?
+    //    
+    //         <div style={{ width: '100%', display: "flex", backgroundColor:"lightblue", justifyContent: "center", paddingTop: "50px"}}>
+    //             <div sx={{ backgroundColor: "green", width: "600px"}}>
+    //                 <Box sx={{ width: 500, margin: '0 auto 16px', backgrounColor:"blue" }}>
+    //                     <Grid container justifyContent="center">
+    //                         <Typography>
+    //                             All Stories
+    //                         </Typography>
+    //                     </Grid>
+    //                 </Box>
+    //                 <Box sx={{ height: 400, bgcolor: 'background.paper' }}>
+    //                     What if I put something else in this box?
+    //                     {/* <DataGridPro
+    //                     apiRef={apiRef}
+    //                     // onCellClick={handleCellClick}
+    //                     hideFooter
+    //                     {...data}
+    //                     /> */}
+    //                 </Box>
+    //             </div>
+    //             <div>
+    //                 <Box sx={{ width: 300, margin: '0 auto 16px' }}>
+    //                     <Grid container justifyContent="center">
+    //                         <Typography>
+    //                             My Stories
+    //                         </Typography>
+                        
+    //                     </Grid>
+    //                 </Box>
+    //                 <Box sx={{ height: 400, bgcolor: 'background.paper' }}>
+    //                     <DataGridPro
+    //                     apiRef={apiRef}
+    //                     // onCellClick={handleCellClick}
+    //                     hideFooter
+    //                     {...data}
+    //                     />
+    //                 </Box>
+    //             </div>
+    //         </div>
+
+    {/* <Grid item>
+            <Button onClick={handleClick('top')}>top</Button>
+          </Grid>
+        </Grid>
+        <Grid container textAlign="center">
+          <Grid item xs={4}>
+            <Button onClick={handleClick('left')}>left</Button>
+          </Grid>
+          <Grid item xs={4}>
+            <IconButton
+              color="primary"
+              aria-label="home"
+              onClick={handleClick('home')}
+            >
+              <HomeIcon /> 
+            </IconButton>
+          </Grid>
+          <Grid item xs={4}>
+            <Button onClick={handleClick('right')}>right</Button>
+          </Grid>
+        </Grid>
+        <Grid container justifyContent="center">
+          <Grid item>
+            <Button onClick={handleClick('bottom')}>bottom</Button>
+          </Grid> */}
+          // const apiRef = useGridApiRef();
+
+    // // const [coordinates, setCoordinates] = React.useState({
+    // //   rowIndex: 0,
+    // //   colIndex: 0,
+    // // });
+  
+    // const { data } = useDemoData({
+    //   dataSet: 'Commodity',
+    //   rowLength: 100,
+    // });
+  
+    // // React.useEffect(() => {
+    // //   const { rowIndex, colIndex } = coordinates;
+    // // //   apiRef.current.scrollToIndexes(coordinates);
+    // //   const id = gridVisibleSortedRowIdsSelector(apiRef.current.state)[rowIndex];
+    // //   const column = visibleGridColumnsSelector(apiRef.current.state)[colIndex];
+    // // //   apiRef.current.setCellFocus(id, column.field);
+    // // }, [apiRef, coordinates]);
+  
+    // // const handleClick = (position) => () => {
+    // //   const maxRowIndex = gridVisibleRowCountSelector(apiRef.current.state) - 1;
+    // //   const maxColIndex = visibleGridColumnsLengthSelector(apiRef.current.state) - 1;
+  
+    // //   setCoordinates((coords) => {
+    // //     switch (position) {
+    // //       case 'top':
+    // //         return { ...coords, rowIndex: Math.max(0, coords.rowIndex - 1) };
+    // //       case 'bottom':
+    // //         return { ...coords, rowIndex: Math.min(maxRowIndex, coords.rowIndex + 1) };
+    // //       case 'left':
+    // //         return { ...coords, colIndex: Math.max(0, coords.colIndex - 1) };
+    // //       case 'right':
+    // //         return { ...coords, colIndex: Math.min(maxColIndex, coords.colIndex + 1) };
+    // //       default:
+    // //         return { ...coords, rowIndex: 0, colIndex: 0 };
+    // //     }
+    // //   });
+    // // };
+  
+    // // const handleCellClick = (params) => {
+    // //   const rowIndex = gridVisibleSortedRowIdsSelector(apiRef.current.state).findIndex(
+    // //     (id) => id === params.id,
+    // //   );
+  
+    // //   const colIndex = visibleGridColumnsSelector(apiRef.current.state).findIndex(
+    // //     (column) => column.field === params.field,
+    // //   );
+  
+    // //   setCoordinates({ rowIndex, colIndex });
+    // // };
+
+    //this was in the original box (that the paper is stored in)
