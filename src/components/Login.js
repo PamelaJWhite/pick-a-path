7d124453608e868516be1059ebc82b48d46dd0a6
pjwhite08 <pamelajeanwhite@gmail.com>
2022-02-02 09:57:22 -0600

    cleaned up and comented login

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,105 +5,60 @@ import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
 
-//Import Context
 import { Context } from "../context/Context";
 
-    const Login = () => {
+//login screen component
+const Login = () => {
+  //destructure context for only the states and functions needed
+  const {
+      isSignedIn,
+      userName,
+      setUserName,
+      password,
+      setPassword,
+      handleLogIn
+  } = useContext(Context);
 
-    //destructure context for only the states and functions needed
-    const {
-        isSignedIn,
-        userName,
-        setUserName,
-        password,
-        setPassword,
-        handleLogIn,
-        setIsSignedIn
-    } = useContext(Context);
-
-    return (
-        // <ThemeProvider theme={theme}>
-            <form 
-                style={{ display: "flex", justifyContent: "center"}}
-            >
-            {/* Dynamicaly render content based on isSignedIn state from context  */}
-                {!isSignedIn && (
-                    <Paper className="login">
-                    <TextField
-                        className="textField"
-                        id="userName_field"
-                        value={userName}
-                        onChange={(e) => setUserName(e.target.value)}
-                        variant="outlined"
-                        placeholder="User Name"
-                    />
-                    {/* <div className={classes.spacer}></div> */}
-                    <TextField
-                        className="textField"
-                        id="password_field"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        variant="outlined"
-                        placeholder="Password"
-                        type="password"
-                    />
-                    {/* <div className={classes.spacer}></div> */}
-                    <Button 
-                        variant="contained" 
-                        onClick={(e) => handleLogIn(e)}
-                    >
-                        Log In
-                    </Button>
-                    <Link 
-                    className="signUpButton"
-                    // style={{ color: "#feeda7"}}
-                    to={`/signUp`}>
-                        Sign Up!
-                    </Link>
-                    </Paper> 
-                )} 
-            </form>
-        // </ThemeProvider>
-        
-    );
+  return (
+    <form 
+      style={{ display: "flex", justifyContent: "center"}}
+    >
+      {/* Dynamicaly render login component if not signed in*/}
+      {!isSignedIn && (
+        <Paper className="login">
+          <TextField
+            className="textField"
+            id="userName_field"
+            //set the value of userName to whatever is being tped
+            onChange={(e) => setUserName(e.target.value)}
+            value={userName}
+            variant="outlined"
+            placeholder="User Name"
+          />
+          <TextField
+              className="textField"
+              id="password_field"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              variant="outlined"
+              placeholder="Password"
+              type="password"
+          />
+          <Button 
+            variant="contained" 
+            onClick={(e) => handleLogIn(e)}
+          >
+            Log In
+          </Button>
+          <Link 
+            className="signUpButton"
+            to={`/signUp`}>
+              Sign Up!
+          </Link>
+        </Paper> 
+      )} 
+    </form>
+  );
 };
 
 export default Login
-// const theme = createTheme({
-//     typography: {
-//         h1: {
-//             fontSize: 50,
-//         },
-//         h2: {
-//             fontSize: 35,
-//         },
-//         h3: {
-//             fontSize: 25,
-//         },
-//         h4: {
-//             fontSize: 17,
-//         },
-//         fontFamily: "Poppins"
-//     },
-//   });
-//create styles for this component
-// const useStyles = makeStyles({
-//     login: {
-//         height: 200,
-//         display: "flex",
-//         flexDirection: "column",
-//         alignItems: "center",
-//         justifyContent: "center",
-//     },
-//     textField: {
-//         width: 300,
-//     },
-//     spacer: {
-//         height: 15,
-//     },
-//     });
-
- //create variable name to access styles
-    //?? do I need this? I think I can do them directly
-    // const classes = useStyles();
-
